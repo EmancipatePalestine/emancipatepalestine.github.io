5917aef9d8ede76a71e06d436f92d1388aaed0b4
Emancipate Palestine <emancipate.palestine@gmail.com>
2024-04-28 19:53:37 -0400

    changed media query to only care about width and not orientation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
-      'phone-landscape': {'raw': '(orientation: landscape)'},
-      'desktop': { 'raw': '(orientation: landscape) and (min-width: 80rem)'},
+      'phone-landscape': {'raw': '(min-width: 50rem)'},
+      'desktop': { 'raw': '(min-width: 100rem)'},
     },
     extend: {
-      textShadow: {
-        sm: '1px 1px 1px var(--tw-shadow-color)',
-        DEFAULT: '2px 2px 1.5px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
     },
   },
   plugins: [
     require("@tailwindcss/typography"), require("daisyui"),
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': (value) => ({
-            textShadow: value,
-          }),
-        },
-        { values: theme('textShadow') }
-      )
-    }),
   ],
 
   daisyui: {
