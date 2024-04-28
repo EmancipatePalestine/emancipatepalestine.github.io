1e9239d40f440bcc98c1b20e574424c13e5f6172
Emancipate Palestine <emancipate.palestine@gmail.com>
2024-04-28 18:58:33 -0400

    first article

--- src/articles.js
+++ src/articles.js
@@ -1,22 +1,14 @@
 const articles = [
     {
-        title: "A title",
-        date: "01/31/2000",
-        description: "lorem ipsum",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Flag_of_Palestine.svg/800px-Flag_of_Palestine.svg.png",
-        alt: "Palestine flag",
+        title: "UN experts condemn 'flour massacre'",
+        date: "29/02/2024",
+        description: "Israeli Occupying Forces unleashed violence massacring 112 people. These people only wished to gather flour.",
+        image: "https://ichef.bbci.co.uk/news/1024/cpsprodpb/623C/production/_132784152_drone2.png.webp",
+        alt: "Israeli IDF drone looking over the massacre",
         articleLinks: [
-            "https://www.google.com",
-            "https://www.duckduckgo.com"
-        ]
-    },{
-        title: "A title",
-        date: "01/31/2000",
-        description: "lorem ipsum",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Flag_of_Palestine.svg/800px-Flag_of_Palestine.svg.png",
-        alt: "Palestine flag",
-        articleLinks: [
-            "https://www.google.com",
+            "https://www.ohchr.org/en/press-releases/2024/03/un-experts-condemn-flour-massacre-urge-israel-end-campaign-starvation-gaza",
+            "https://www.nytimes.com/live/2024/02/29/world/israel-hamas-war-gaza-news",
+            "https://www.bbc.com/news/world-middle-east-68445973"
         ]
     }
 ]
